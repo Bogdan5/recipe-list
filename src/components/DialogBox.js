75c3bb32fd7b2aa8a88be6500cb3382205840cca
Bogdan5 <bogdan5x@gmail.com>
2018-05-02 21:09:14 -0400

    Comment

--- src/components/DialogBox.js
+++ src/components/DialogBox.js
@@ -5,7 +5,7 @@ import '../App.css';
 class DialogBox extends Component {
   constructor(props) {
     super(props);
-    this.state = { name: '', ingredients: '' };
+    this.state = { name: '', ingredients: '', dimensions: { top: -1 } };
   }
 
   hideDB = () => {
@@ -21,26 +21,35 @@ class DialogBox extends Component {
   getValueIngredients = (e) => { this.setState({ ingredients: e.target.value });};
 
   render() {
+    console.log(this.state.dimensions);
+    const { top } = this.state.dimensions;
     return (
         <div className="DialogBox zInvisible">
-          <div className="DialogDiv">
-            <header className="header-db">
-              <div>Add a recipe</div>
-              <div onClick={this.hideDB}>x</div>
-            </header>
-            <form>
-              <p>Recipe</p>
-              <input type="text" name="recipeName" value={this.state.name}
-              onChange={this.getValueName}/>
-              <p>Ingredients</p>
-              <input type="text" name="ingredients" value={this.state.ingredients}
-              onChange={this.getValueIngredients}/>
-            </form>
-            <footer>
-              <input type="submit" value="Submit" onClick={this.addRecipe}/>
-              <button onClick={this.hideDB}>Cancel</button>
-            </footer>
-          </div>
+          <Measure bounds onResize={(contentRect) => {
+            this.setState({ dimensions: contentRect.bounds });
+          }
+          }>
+            {({ measureRef }) => { <div className="DialogDiv" ref={ measureRef }>
+              <header className="header-db">
+                <div>Add a recipe</div>
+                <div onClick={this.hideDB}>x</div>
+              </header>
+              <form>
+                <p>Recipe</p>
+                <input type="text" name="recipeName" value={this.state.name}
+                onChange={this.getValueName}/>
+                <p>Ingredients</p>
+                <input type="text" name="ingredients" value={this.state.ingredients}
+                onChange={this.getValueIngredients}/>
+              </form>
+              <footer>
+                <input type="submit" value="Submit" onClick={this.addRecipe}/>
+                <button onClick={this.hideDB}>Cancel</button>
+              </footer>
+            </div>;
+            }
+          }
+          </Measure>
         </div>
     );
   }
